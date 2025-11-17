c7de31b1ffa7b430432bbdf1b8c7b28aded057a2
BrandonAWong <brandon.alex.wong@gmail.com>
2025-11-16 22:21:42 -0800

    add quantity default 1

--- api/routers/nutrition.py
+++ api/routers/nutrition.py
@@ -40,7 +40,9 @@ async def get_nutrition(items: list[Item]):
             i.carbs = result.nutrition.carbohydrates_total
             i.fats = result.nutrition.total_fat_total
             i.protein = result.nutrition.protein_total
+            print(f"Nutirion hit: {i.name}")
         except:
             print(f"Validation Error: no nutrition found for item - {i.name}")
-        
+
+    print("analsyis complete")
     return items
